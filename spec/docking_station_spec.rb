9163e77322f73eac6db52d3efc2aba9c4424027e
gabe <GabeMaker@users.noreply.github.com>
2015-07-07 12:51:30 +0100

    Freed real Bikes by substituting in doubles.

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -9,14 +9,13 @@ describe DockingStation do
   end
 
   it 'should release a working bike' do
-    subject.dock Bike.new
-    bike = subject.release_bike
-    expect(bike).to be_working
+    bike = double :bike, working?: true
+    subject.dock bike
+    expect(subject.release_bike).to be bike
   end
 
   it 'should not release a broken bike' do
-    bike = Bike.new
-    bike.report_broken
+    bike = double :bike, working?: false
     subject.dock bike
     expect { subject.release_bike }.to raise_error 'No bikes available'
   end
@@ -31,8 +30,8 @@ describe DockingStation do
 
   describe 'dock' do
     it 'raises an error when full' do
-      subject.capacity.times { subject.dock Bike.new }
-      expect { subject.dock Bike.new }.to raise_error 'Docking station full'
+      subject.capacity.times { subject.dock double :bike }
+      expect { subject.dock double :bike }.to raise_error 'Docking station full'
     end
   end
 
