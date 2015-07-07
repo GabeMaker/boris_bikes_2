80026b93d24dad8be93405147b8cc222576b6685
gabe <GabeMaker@users.noreply.github.com>
2015-07-07 10:55:42 +0100

    create capacity attribute

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,17 +1,34 @@
 require_relative 'bike'
 
 class DockingStation
+  DEFAULT_CAPACITY = 20
+
+  attr_reader :capacity
+
   def initialize
+    @capacity = DEFAULT_CAPACITY
     @bikes = []
   end
 
   def release_bike
-    fail 'No bikes available.' if @bikes.empty?
-    @bikes.pop
+    fail 'No bikes available.' if empty?
+    bikes.pop
   end
 
   def dock bike
-    fail 'Docking station full' if @bikes.length >= 20
-    @bikes << bike
+    fail 'Docking station full' if full?
+    bikes << bike
+  end
+
+  private
+
+  attr_reader :bikes
+
+  def empty?
+    bikes.empty?
+  end
+
+  def full?
+    bikes.length >= capacity
   end
 end
