18b818321269b6fd43d1aa73d33c5ba1678cc3f2
gabe <GabeMaker@users.noreply.github.com>
2015-07-08 20:58:26 +0100

    garage can dock bikes

--- lib/garage.rb
+++ lib/garage.rb
@@ -1,7 +1,18 @@
 class Garage
 
-  def fix_bikes
-
+  def initialize
+    @bikes = []
   end
+
+  def fix_bikes
+  end
+
+  def dock bike
+    bikes << bike
+  end
+
+  private
+
+  attr_reader :bikes
 
 end
--- spec/garage_spec.rb
+++ spec/garage_spec.rb
@@ -1,7 +1,18 @@
 require 'garage'
 
+# helper method
+class Garage
+  attr_reader :bikes
+end
+
 describe Garage do
 
   it { is_expected.to respond_to :fix_bikes }
+
+  it 'it can accept bikes' do
+    bike = double :bike
+    subject.dock bike
+    expect(subject.bikes).to eq [bike]
+  end
 
 end
