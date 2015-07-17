48d3b725c1c9a286becc9540b5d085c60c6a5aff
gabe <GabeMaker@users.noreply.github.com>
2015-07-17 12:53:22 +0100

    station double returns broken_bike in array when it receives #release_broken_bikes

--- spec/van_spec.rb
+++ spec/van_spec.rb
@@ -6,14 +6,14 @@ describe Van do
 
   it 'should be able to take broken bikes from a docking station' do
     broken_bike = double :broken_bike, working?: false
-    station = double :station, release_broken_bikes: broken_bike
+    station = double :station, release_broken_bikes: [broken_bike]
     subject.get_bikes_from(station)
     expect(subject.bikes).to eq [broken_bike]
   end
 
   it 'can give broken bikes to a garage' do
     broken_bike = double :broken_bike, working?: false
-    station = double :station, release_broken_bikes: broken_bike
+    station = double :station, release_broken_bikes: [broken_bike]
     subject.get_bikes_from(station)
     garage = double :garage, bikes: []
     subject.give_bikes_to(garage)
